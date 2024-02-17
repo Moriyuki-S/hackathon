ac3f5d390dddd5b48c64ddbaa0c56669d5addb56
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-02-17 19:06:28 +0900

    Add :  NextUIを導入

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js'],
   theme: {
     screens: {
       sm: "380px",
